af7f312986f6149d438b09b1f101348d95703a89
RICHARD FIAGBEATI <Ausbel@users.noreply.huggingface.co>
2023-07-25 00:43:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,48 @@
-#Create the Gradio app interface
+
 import gradio as gr
-from transformers import pipeline
-#from transformers import AutoTokenizer
-#tokenizer = AutoTokenizer.from_pretrained('Sonny4Sonnix/twitter-roberta-base-sentimental-analysis-of-covid-tweets')
-#from transformers import RobertaForSequenceClassification
-#from transformers import AutoModelForSequenceClassification
 
-#sentiment = pipeline("/content/drive/MyDrive/Colab Notebooks/Twitter_roBERTa_base_for_Sentiment_Analysis.ipynb")
+import transformers as pipeline
 
-sentiment = pipeline("sentiment-analysis",model ="Ausbel/Vaccine-tweet-sentiments-analysis-model-2")
-def get_sentiment(iput_text):
-    return sentiment(input_text)
+from transformers import AutoTokenizer,AutoModelForSequenceClassification
 
-results = get_sentiment("Covid is very dangerous")
-results
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
-iface = gr.Interface(fn=get_sentiment, input=gr.inputs.Textbox(),outputs="text")
-iface.launch()
+ 
+
+model_name = "Ausbel/Vaccine-tweet-sentiments-analysis-model-2" 
+
+# Replace with the name of the pre-trained model you want to use
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+sentiment = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+def get_sentiment(input_text):
+
+        return sentiment(input_text)
+
+ 
+
+
+
+# #Function to predict sentiments from the input text using the model
+
+#     prediction = model.predict([text])[0]
+
+#     if label==-1:
+
+#        return "Negative"
+
+#     elif label== 0:
+
+#         return "Neutral"
+
+#     else:
+
+#         return "Positive"
+
+ 
+
+iface = gr.Interface(fn=get_sentiment,title="Sentimental Analysis", inputs="text",outputs="text")
+
+iface.launch(inline=True)
